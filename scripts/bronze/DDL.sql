6162670988fed7b57c6d608328ff8e526075888b
rushi_lahane <rushieconomist@gmail.com>
2025-05-04 19:44:36 +0530

    Update DDL.sql

--- scripts/bronze/DDL.sql
+++ scripts/bronze/DDL.sql
@@ -1,12 +1,10 @@
 /*
-===============================================================================
-DDL Script: Create Bronze Tables
-===============================================================================
+-------------------------------------------------------------------------------
 Script Purpose:
     This script creates tables in the 'bronze' schema, dropping existing tables 
     if they already exist.
 	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
+-------------------------------------------------------------------------------
 */
 
 DROP TABLE IF EXISTS bronze.crm_cust_info
